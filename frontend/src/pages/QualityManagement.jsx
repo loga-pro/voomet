23dd1d57e638b1b251f2c1492d5eeb6ec02ae11f
ItCaldim <softwareit@caldimengg.in>
2025-12-31 14:55:50 +0530

    31-12 14:55

--- frontend/src/pages/QualityManagement.jsx
+++ frontend/src/pages/QualityManagement.jsx
@@ -237,7 +237,7 @@ const QualityManagement = () => {
       'Remarks',
       'Created Date',
       'Last Updated',
-      // Quality Issues Details
+  
       'Issue #',
       'Issue Date',
       'Issue Scope',
